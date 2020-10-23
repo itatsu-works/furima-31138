6f801a40d1f1c201b4a84e8591884d13b567b189
itatsu-works <itatsu.0909@gmail.com>
2020-10-23 16:56:43 +0900

    Rubocop実施後

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {Faker::Name.initials(number: 5)}
-    email                   {Faker::Internet.free_email}
-    password                {Faker::Internet.password(min_length: 6)}
-    password_confirmation   {password}
-    last_name               {"阿部"}    
-    first_name              {"慎之助"}
-    last_kana               {"アベ"}
-    first_kana              {"シンノスケ"}
-    birthday                {Faker::Date.birthday(min_age: 17, max_age: 70)}
+    nickname                { Faker::Name.initials(number: 5) }
+    email                   { Faker::Internet.free_email }
+    password                { Faker::Internet.password(min_length: 6) }
+    password_confirmation   { password }
+    last_name               { '阿部' }
+    first_name              { '慎之助' }
+    last_kana               { 'アベ' }
+    first_kana              { 'シンノスケ' }
+    birthday                { Faker::Date.birthday(min_age: 17, max_age: 70) }
   end
 end
