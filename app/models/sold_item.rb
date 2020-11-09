3a7fe93ffd6b717794c0fb34d959f722c8ab4242
itatsu-works <itatsu.0909@gmail.com>
2020-11-09 14:37:46 +0900

    [修正]エラーメッセージ内容修正

--- app/models/sold_item.rb
+++ app/models/sold_item.rb
@@ -19,16 +19,22 @@ class SoldItem
     validates :city
     validates :address
     validates :phone_number
-    validates :token
   end
-  
+
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :phone_number, format: {with: /\A\d{10,11}\z/ }
 
+  validate :add_error_token
+
+  def add_error_token
+    if token.blank?
+      errors[:base] << "Please re-enter your card information"
+    end
+  end
+
   def save
     sold = Sold.create(item_id:item_id,  user_id: user_id)
     Delivery.create(post_number:  post_number, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, sold_id: sold.id)
-
   end
 end
